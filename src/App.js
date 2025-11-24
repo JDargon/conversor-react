29699993aa918a87577e700ca415fa487467a939
JDargon <argon181120@gmail.com>
2025-11-24 21:48:52 +0000

    feat: prueba de comunicacion. Usa console.log

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import { useRef, useState } from "react"
+import { useRef, useState, useEffect } from "react"
 import './App.css'
 
 function App() {
 
-  const[valorCambio, setValorCambio]=useState(null);
+  const [valorCambio, setValorCambio] = useState(null);
   const eurosRef = useRef();
   const resultadoRef = useRef();
 
-  const calcular=()=>{
-    const eurosValor=parseFloat(eurosRef.current.value);
-    const dolares=eurosValor*1.15;
-    resultadoRef.current.innerHTML=dolares.toFixed(2) + "$";
+  useEffect(() => {
+
+    const llamaApiCambio = async () => {
+
+      try {
+
+        const respuesta = await fetch("https://v6.exchangerate-api.com/v6/46b4858915e04d322817be31/latest/USD");
+        const datos = await respuesta.json();
+        console.log(datos);
+
+      } catch (error) {
+        console.error("Error al acceder a la API", error);
+      }
+
+    };
+
+    llamaApiCambio();
+
+  }, []);
+
+  const calcular = () => {
+    const eurosValor = parseFloat(eurosRef.current.value);
+    const dolares = eurosValor * 1.15;
+    resultadoRef.current.innerHTML = dolares.toFixed(2) + "$";
   }
 
   return <div>
     <h1>Conversor Euro-Dólar</h1>
-    <input className="centrarElementos" type="text" ref={eurosRef}></input><br/>
-    <button className="centrarElementos" onClick={calcular}>Convertir</button><br/>
+    <input className="centrarElementos" type="text" ref={eurosRef}></input><br />
+    <button className="centrarElementos" onClick={calcular}>Convertir</button><br />
     <div className="centrarElementos resultado" ref={resultadoRef}></div>
-    </div>
+  </div>
 }
 
 export default App;
